b2f27a6a7241a5b9c66a9fb226c11e4e544c9584
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-05-29 08:44:52 +0530

    ready_to_deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,36 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.NODE_ENV}`],
+      validationSchema: configValidationSchema,
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'vijay',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'prod';
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_username'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_databases'),
+        };
+      },
     }),
     AuthModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Main', true);
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe()); // no need to write at controller level
   app.useGlobalInterceptors(new TransformInterceptor());
-  const PORT = 3000;
+  const PORT = process.env.PORT;
   await app.listen(PORT, () => {
     logger.log(`Application listening in port ${PORT}`);
   });
