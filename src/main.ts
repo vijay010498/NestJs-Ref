a4e904c443341e6d2e279fb0aa9dce3d58bccff0
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-05-29 09:33:48 +0530

    added_multi_clustering

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,33 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
+import * as cluster from 'cluster';
+import * as os from 'os';
+const numCpu = os.cpus().length;
 
 async function bootstrap() {
-  const logger = new Logger('Main', true);
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe()); // no need to write at controller level
-  app.useGlobalInterceptors(new TransformInterceptor());
-  const PORT = process.env.PORT;
-  await app.listen(PORT, () => {
-    logger.log(`Application listening in port ${PORT}`);
-  });
+  if (cluster.isMaster) {
+    console.log(`I am the master I will create ${numCpu} worker's`);
+    for (let i = 0; i < numCpu; i++) {
+      cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(
+        `worker ${worker.process.pid} dead with code ${code} and signal:${signal}`,
+      );
+      cluster.fork();
+      console.log('Died Thread started');
+    });
+  } else {
+    const logger = new Logger('Main', true);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe()); // no need to write at controller level
+    app.useGlobalInterceptors(new TransformInterceptor());
+    const PORT = process.env.PORT;
+    await app.listen(PORT, () => {
+      logger.log(`worker "${process.pid}" started on port ${PORT}`);
+    });
+  }
 }
 bootstrap();
